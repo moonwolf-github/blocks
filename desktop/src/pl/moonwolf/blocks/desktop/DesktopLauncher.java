223f7b30312815f67b1da0a52a2e3af5cda04d46
Marcin Trendota <marcin@trendota.com>
2017-12-20 17:09:15 +0100

    Initial commit

---
+++ desktop/src/pl/moonwolf/blocks/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package pl.moonwolf.blocks.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import pl.moonwolf.blocks.Blocks;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Blocks(), config);
+	}
+}
