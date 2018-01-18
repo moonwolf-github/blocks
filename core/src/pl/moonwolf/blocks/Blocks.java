99bc55f3854ce0289a769b3659ffdd1637414420
Marcin Trendota <marcin@trendota.com>
2018-01-18 20:16:56 +0100

    Fix intendation

--- core/src/pl/moonwolf/blocks/Blocks.java
+++ core/src/pl/moonwolf/blocks/Blocks.java
@@ -12,32 +12,32 @@ import pl.moonwolf.blocks.screens.MainScreen;
 
 public class Blocks extends Game
 {
-	private SpriteBatch batch;
+    private SpriteBatch batch;
     private Viewport viewport;
-	public static final float VIRTUAL_HEIGHT = 10;
+    public static final float VIRTUAL_HEIGHT = 10;
     public static final float VIRTUAL_WIDTH = 10;
 
     @Override
-	public void create ()
+    public void create ()
     {
-		viewport = new ExtendViewport(800, 600);
-		((OrthographicCamera) this.viewport.getCamera()).setToOrtho(false, Blocks.VIRTUAL_WIDTH, Blocks.VIRTUAL_HEIGHT);
-		batch = new SpriteBatch();
+        viewport = new ExtendViewport(800, 600);
+        ((OrthographicCamera) this.viewport.getCamera()).setToOrtho(false, Blocks.VIRTUAL_WIDTH, Blocks.VIRTUAL_HEIGHT);
+        batch = new SpriteBatch();
         batch.setProjectionMatrix(((OrthographicCamera) this.viewport.getCamera()).combined);
-		setScreen(new MainScreen(batch, viewport));
-	}
+        setScreen(new MainScreen(batch, viewport));
+    }
 
-	@Override
-	public void render ()
+    @Override
+    public void render ()
     {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         super.render();
-	}
-	
-	@Override
-	public void dispose ()
+    }
+
+    @Override
+    public void dispose ()
     {
-		batch.dispose();
-	}
+        batch.dispose();
+    }
 }
